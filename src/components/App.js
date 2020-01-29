fc29d477fae4f32a236a1ae7ca96f57a71c1542d
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-01-29 09:57:33 -0800

    clean up code and pass app.test

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable react/jsx-key */
 import React from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
@@ -7,15 +9,17 @@ const chance = new Chance();
 import styles from './Character.css';
 
 const characters = [...Array(8)]
-  .map(i => ({name: chance.name(), image: "http://placegoat.com/300/300", status: chance.pickone(['alive', 'dead', 'unknown']), species: chance.animal(), gender: chance.pickone(['male', 'female', 'nonbinary']), origin: chance.city(), lastLocation: chance.city() }))
+  .map(i => ({ name: chance.name(), image: 'http://placegoat.com/300/300', status: chance.pickone(['alive', 'dead', 'unknown']), species: chance.animal(), gender: chance.pickone(['male', 'female', 'nonbinary']), origin: chance.city(), lastLocation: chance.city() }));
 
 console.log(characters);
 
 const App = () => (
   <React.Fragment>
     <Header />
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
     <div className={styles.container}>
-    {characters.map(character => <Character image={character.image} name={character.name} status={character.status} species={character.species} gender={character.gender} origin={character.origin} lastLocation={character.lastLocation}/>)};
+      // eslint-disable-next-line react/jsx-key
+      {characters.map(character => <Character image={character.image} name={character.name} status={character.status} species={character.species} gender={character.gender} origin={character.origin} lastLocation={character.lastLocation}/>)};
     </div>
     <Footer />
   </React.Fragment>
